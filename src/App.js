ebc23341bcd5d7d8f8e8fe44467cb82269a3b10f
Sourav Kumar Bhunia <souravbhunia400@gmail.com>
2023-10-05 12:35:09 +0530

    Frontend Intern Task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,248 @@
-import logo from './logo.svg';
 import './App.css';
+import logo_given from "./images/logo_given.svg";
+import template from "./images/template.svg";
+import interview from "./images/interview.svg";
+import chatbot from "./images/chatbot-icon.svg";
+import practiceTest from "./images/interview-svgrepo-com.svg";
+import readme from "./images/readme.svg";
+import setting from "./images/Settings.svg";
+import group from "./images/Group.svg";
+import crown from "./images/crown.svg";
+import resume from "./images/resume.svg";
+import course from "./images/online-course.svg";
+import user from "./images/User_01.svg";
+import logout from "./images/LogOut.svg";
+import chart from "./images/chart.png";
+import arrowForward from "./images/arrowforward.png";
+import ellipse from "./images/ellipse-863.png";
+import criticalThinking from "./images/critical-thinking.png";
+import timeManamgement from "./images/time-management.png";
+import focusManagement from "./images/focus-management.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="writing-test">
+      <div class="result">
+        <div class="correct_ans">
+          <div class="correct_ans_div">
+            <div class="result_bg">
+            </div>
+          </div>
+          <div class="ans_legend">
+            <div class="answer_col">
+              <div class="correct_answer_bg">
+              </div>
+              <div class="not_answered">
+              </div>
+              <div class="wrong_ans_bg">
+              </div>
+            </div>
+            <div class="ans_chart">
+              <p class="correct_ans_col">Correct Answer</p>
+              <p class="not_answer_col">Not Answered</p>
+              <p class="wrong_answer_col">Wrong</p>
+            </div>
+          </div>
+        </div>
+        <p class="score-74-XFP">Score: 74%</p>
+        <img class="correct_ans_chart" src={chart} alt="chart"/>
+        <p class="results">Results</p>
+        <div class="result_table">
+          <p class="analysis">Analysis</p>
+          <div class="rectangle_space">
+          </div>
+          <p class="correct-answers">Correct Answers:</p>
+          <p class="wrong-answers">Wrong Answers:</p>
+          <p class="unanswered">Unanswered:</p>
+          <p class="skipped">Skipped:</p>
+          <p class="out-of-time">Out of time:</p>
+          <p class="corr_ans">9</p>
+          <p class="wrn_ans">1</p>
+          <p class="unans_score">1</p>
+          <p class="skip_score">0</p>
+          <p class="out_of_time_score">0</p>
+          <p class="time_used_score">2:30</p>
+          <p class="time_used">Time used:</p>
+          <div class="corr_space">
+          </div>
+          <div class="wrong_space">
+          </div>
+          <div class="unanswered_space">
+          </div>
+          <div class="skipped_space">
+          </div>
+          <div class="out_of_time_space">
+          </div>
+          <div class="time_used_space">
+          </div>
+        </div>
+      </div>
+  <p class="management-score">Management Score:</p>
+  <div class="management-score_space">
+  </div>
+  <p class="writing-test">Writing Test 7</p>
+  <p class="date_time">9 July 2023 at 01:47</p>
+  <div class="side_bar">
+    <div class="side_bar_div">
+      <div class="side_bar_component">
+        <img class="logo" src={logo_given} alt="Logo"/>
+        <div class="dashboard">
+          <img class="template" src={template} alt="dashboard"/>
+          <p class="dashboard-text">Dashboard</p>
+        </div>
+        <div class="mentor_group">
+          <img class="interview_mentor" src={interview} alt="interview"/>
+          <p class="on-1-mentor">1 on 1 mentor</p>
+        </div>
+        <div class="mock_interview_group">
+          <img class="chatbot-icon" src={chatbot} alt="mock_interview"/>
+          <p class="mock-interviews">Mock Interviews</p>
+        </div>
+        <div class="practice_test_group">
+          <img class="practice_test_icon" src={practiceTest} alt="Practice Test"/>
+          <p class="practice_test_text">Practice Test</p>
+        </div>
+        <div class="communication_group">
+          <img class="readme-icon" src={readme} alt="communication"/>
+          <p class="communication-text">Communication</p>
+        </div>
+        <div class="report_group">
+          <div class="group_div">
+            <img class="group_icon" src={group} alt="report"/>
+            <p class="reports-text">Reports</p>
+          </div>
+        </div>
+        <div class="settings_group">
+          <img class="settings_icon" src={setting} alt="setting"/>
+          <p class="settings_text">Settings</p>
+        </div>
+        <div class="premium_group">
+          <img class="premium_icon" src={crown} alt="premium icon"/>
+          <p class="premium_text">Premium</p>
+        </div>
+      </div>
+      <div class="empty_line">
+      </div>
+      <div class="resume_section">
+        <div class="resume_group">
+          <img class="resume_cv_icon" src={resume} alt="resume_icon"/>
+          <p class="resume_text">Resume</p>
+        </div>
+        <div class="courses_group">
+          <img class="courses_icon" src={course} alt="course_icon"/>
+          <p class="courses_text">Courses</p>
+        </div>
+      </div>
     </div>
+    <div class="text_field">
+      <div class="answer_field">
+        <div class="answer_group">
+          <p class="your_answer_text">Your Answer:</p>
+          <div class="answer_text_1">
+            <p class="answer_part_1_text">
+              <span class="answer_part_1_text-sub-0">The most challenging software for me is specifically to be specifically Telluride is Figma. Yeah. Figma and Adobe Illustrator, which is, </span>
+              <span class="answer_part_1_text-sub-1">uh</span>
+              <span class="answer_part_1_text-sub-2">,</span>
+              <span class="answer_part_1_text-sub-3"> they are quite </span>
+              <span class="answer_part_1_text-sub-4">nuance and</span>
+              <span class="answer_part_1_text-sub-5"> all the constraints and, </span>
+              <span class="answer_part_1_text-sub-6">uh</span>
+              <span class="answer_part_1_text-sub-7"> spill small, small things that needs to be learned up. Yeah. This question, it almost took </span>
+              <span class="answer_part_1_text-sub-8">three to four</span>
+              <span class="answer_part_1_text-sub-9"> months to </span>
+              <span class="answer_part_1_text-sub-10">complete</span>
+              <span class="answer_part_1_text-sub-11"> a entire software of the Adobe Illustrator.</span>
+            </p>
+          </div>
+          <p class="answer_part_2_text">
+            <span class="answer_part_2_text-sub-0">The most challenging software for me is specifically to be specifically Telluride is Figma. Yeah. Figma and Adobe Illustrator, which is, </span>
+            <span class="answer_part_2_text-sub-1">uh</span>
+            <span class="answer_part_2_text-sub-2">,</span>
+            <span class="answer_part_2_text-sub-3"> they are quite nuance and all the constraints and, </span>
+            <span class="answer_part_2_text-sub-4">uh</span>
+            <span class="answer_part_2_text-sub-5"> spill small, small things that needs to be learned up. Yeah. This question, it almost took three to four months to complete a entire software of the Adobe Illustrator.</span>
+          </p>
+          <p class="answer_part_3_text">
+            <span class="answer_part_3_text-sub-0">The most challenging software for me is specifically to be specifically Telluride is Figma. Yeah. Figma and Adobe Illustrator, which is, </span>
+            <span class="answer_part_3_text-sub-1">uh</span>
+            <span class="answer_part_3_text-sub-2">,</span>
+            <span class="answer_part_3_text-sub-3"> they are quite nuance and all the constraints and, </span>
+            <span class="answer_part_3_text-sub-4">uh</span>
+            <span class="answer_part_3_text-sub-5"> spill small, small things that needs to be learned up.</span>
+          </p>
+          <div class="error_legend">
+            <div class="grammatical_error_red">
+            </div>
+            <p class="grammatical_error_text">Grammatical error</p>
+            <div class="vocabulary_error_yellow">
+            </div>
+            <p class="vocabulary_error_text">Vocabulary error</p>
+            <div class="spelling_error_blue">
+            </div>
+            <p class="spelling_error_text">Spelling error</p>
+          </div>
+        </div>
+        <div class="answer_bottom_line">
+        </div>
+        <div class="management_score_section">
+          <div class="focus_management_score_section">
+            <div class="focus_score">
+              <p class="focus_score_text">59</p>
+              <p class="focus_score_precentage_text">Score: 59%</p>
+            </div>
+            <div class="focus_management_text_section">
+              <p class="focus_management_text">Focus Management</p>
+              <p class="focus_management_text_info">Focus Score: Focus score refers to the ability of an individual to concentrate and direct their attention towards a specific task or information. </p>
+            </div>
+          </div>
+          <div class="focus_management_bottom_line">
+          </div>
+          <div class="time_management_score_section">
+            <div class="time_score">
+              <p class="time_score_text">89</p>
+              <p class="time_score_precentage_text">Score: 74%</p>
+            </div>
+            <div class="time_management_text_section">
+              <p class="time_management_text">Time Management</p>
+              <p class="time_management_text_info">Time Management: Time management is the practice of planning, organizing, and allocating time to tasks and activities in a way that maximizes productivity and efficiency. </p>
+            </div>
+          </div>
+          <div class="time_management_bottom_line">
+          </div>
+          <p class="critical_thinking_text">Critical Thinking</p>
+        </div>
+      </div>
+      <div class="focus_management_logo_div">
+        <img class="focus_management_logo" src={focusManagement} alt="focus_management_logo"/>
+      </div>
+      <div class="time_management_div">
+        <img class="time_management_logo" src={timeManamgement} alt="time_management_icon"/>
+      </div>
+    </div>
+    <img class="logout_logo" src={logout} alt="logout_logo"/>
+    <p class="user_name">
+      Josephine 
+      <br/>
+      Langford
+    </p>
+    <div class="rectangular_line_div">
+    </div>
+    <img class="user_icon" src={user} alt="user_icon"/>
+    <div class="rectangle_space_div">
+    </div>
+  </div>
+  <div class="results-texts_space_div">
+  </div>
+  <p class="results-texts">Results</p>
+  <p class="critical_thinking_perentage">Score: 39%</p>
+  <div class="critical_thinking_icon_div">
+    <img class="critical_thinking_icon" src={criticalThinking} alt="critical_thinking_icon"/>
+  </div>
+  <p class="critical_thinking_score">39</p>
+  <p class="critical_thinking">Critical thinking is the process of analyzing, evaluating, and interpreting information or situations in a logical and systematic manner.</p>
+  <img class="circular_down_arrow" src={ellipse} alt="circular_down_arrow"/>
+  <img class="arrowforward-icon" src={arrowForward} alt="arrow_forward"/>
+</div>
   );
 }
 
